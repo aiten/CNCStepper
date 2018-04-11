b406a23545694dddb595b475bcb1c4d656b1dd8c
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2018-04-11 21:57:52 +0200

    Step/Timer ISR for Due

--- Sketch/libraries/StepperLib/src/Steppers/StepperMash6050S.h
+++ Sketch/libraries/StepperLib/src/Steppers/StepperMash6050S.h
@@ -26,6 +26,12 @@
 ////////////////////////////////////////////////////////
 
 #define MASH6050S_ENDSTOPCOUNT 4
+#define MASH6050S_CHANGEDIRECTIONMICROS	5
+
+#if defined(__SAM3X8E__)
+#define USESTEPTIMER
+#define STEPTIMERDELAYINMICRO 2
+#endif
 
 ////////////////////////////////////////////////////////
 
@@ -40,7 +46,6 @@ public:
 	CStepperMash6050S()
 	{
 		_num_axis = 4;
-		_lastStepDirection = 0;
 	}
 
 	////////////////////////////////////////////////////////
@@ -67,22 +72,12 @@ public:
 		CHAL::pinModeOutput(MASH6050S_C_DIR_PIN);
 		CHAL::pinMode(MASH6050S_C_MIN_PIN, MASH6050S_INPUTPINMODE);
 
-#ifdef _MSC_VER
-#pragma warning( disable : 4127 )
-#endif
 
-		HALFastdigitalWrite(MASH6050S_X_STEP_PIN, MASH6050S_PIN_STEP_ON);
-		HALFastdigitalWrite(MASH6050S_Y_STEP_PIN, MASH6050S_PIN_STEP_ON);
-		HALFastdigitalWrite(MASH6050S_Z_STEP_PIN, MASH6050S_PIN_STEP_ON);
-		HALFastdigitalWrite(MASH6050S_C_STEP_PIN, MASH6050S_PIN_STEP_ON);
+		ClearStepPin();
+		SetDirection(0);
 
-		HALFastdigitalWrite(MASH6050S_X_DIR_PIN, MASH6050S_PIN_DIR_OFF);
-		HALFastdigitalWrite(MASH6050S_Y_DIR_PIN, MASH6050S_PIN_DIR_OFF);
-		HALFastdigitalWrite(MASH6050S_Z_DIR_PIN, MASH6050S_PIN_DIR_OFF);
-		HALFastdigitalWrite(MASH6050S_C_DIR_PIN, MASH6050S_PIN_DIR_OFF);
-
-#ifdef _MSC_VER
-#pragma warning( default : 4127 )
+#ifdef USESTEPTIMER
+		CHAL::InitTimer2OneShot(HandleStepPinInterrupt);
 #endif
 	}
 
@@ -90,13 +85,13 @@ protected:
 
 	////////////////////////////////////////////////////////
 
-	virtual void  SetEnable(axis_t , uint8_t , bool ) override
+	virtual void  SetEnable(axis_t, uint8_t, bool) override
 	{
 	}
 
 	////////////////////////////////////////////////////////
 
-	virtual uint8_t GetEnable(axis_t ) override
+	virtual uint8_t GetEnable(axis_t) override
 	{
 		return LevelMax;
 	}
@@ -105,68 +100,162 @@ protected:
 
 #if defined(__SAM3X8E__) || defined(__SAMD21G18A__)
 
-static void Delay1() ALWAYSINLINE { CHAL::DelayMicroseconds(2); }
-static void Delay2() ALWAYSINLINE { CHAL::DelayMicroseconds(2); }
+	static void Delay1() ALWAYSINLINE { CHAL::DelayMicroseconds(2); }
+	static void Delay2() ALWAYSINLINE { CHAL::DelayMicroseconds(2); }
 
 #else //AVR
 
-static void Delay1() ALWAYSINLINE { CHAL::DelayMicroseconds(1); }
-static void Delay2() ALWAYSINLINE { CHAL::DelayMicroseconds(1); }
+	static void Delay1() ALWAYSINLINE { CHAL::DelayMicroseconds(1); }
+	static void Delay2() ALWAYSINLINE { CHAL::DelayMicroseconds(1); }
 
 #endif
 
 	////////////////////////////////////////////////////////
 
-	virtual void Step(const uint8_t steps[NUM_AXIS], axisArray_t directionUp) override
+	static void SetDirection(axisArray_t directionUp)
 	{
-		// Step:   LOW to HIGH
-		// PULS must be at least 1ms
-		// DIRCHANGE must be at least 5ms
-
 		if ((directionUp&(1 << X_AXIS)) != 0) HALFastdigitalWriteNC(MASH6050S_X_DIR_PIN, MASH6050S_PIN_DIR_OFF); else HALFastdigitalWriteNC(MASH6050S_X_DIR_PIN, MASH6050S_PIN_DIR_ON);
 		if ((directionUp&(1 << Y_AXIS)) != 0) HALFastdigitalWriteNC(MASH6050S_Y_DIR_PIN, MASH6050S_PIN_DIR_OFF); else HALFastdigitalWriteNC(MASH6050S_Y_DIR_PIN, MASH6050S_PIN_DIR_ON);
 		if ((directionUp&(1 << Z_AXIS)) != 0) HALFastdigitalWriteNC(MASH6050S_Z_DIR_PIN, MASH6050S_PIN_DIR_OFF); else HALFastdigitalWriteNC(MASH6050S_Z_DIR_PIN, MASH6050S_PIN_DIR_ON);
 		if ((directionUp&(1 << A_AXIS)) != 0) HALFastdigitalWriteNC(MASH6050S_C_DIR_PIN, MASH6050S_PIN_DIR_OFF); else HALFastdigitalWriteNC(MASH6050S_C_DIR_PIN, MASH6050S_PIN_DIR_ON);
+	}
 
-		if (_lastStepDirection != directionUp)
+	////////////////////////////////////////////////////////
+
+	static void SetStepPin(const uint8_t steps[NUM_AXIS], uint8_t cnt)
+	{
+		if (steps[X_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_X_STEP_PIN, MASH6050S_PIN_STEP_OFF); }
+		if (steps[Y_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_Y_STEP_PIN, MASH6050S_PIN_STEP_OFF); }
+		if (steps[Z_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_Z_STEP_PIN, MASH6050S_PIN_STEP_OFF); }
+		if (steps[A_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_C_STEP_PIN, MASH6050S_PIN_STEP_OFF); }
+	}
+
+	////////////////////////////////////////////////////////
+
+	static void ClearStepPin()
+	{
+		HALFastdigitalWriteNC(MASH6050S_X_STEP_PIN, MASH6050S_PIN_STEP_ON);
+		HALFastdigitalWriteNC(MASH6050S_Y_STEP_PIN, MASH6050S_PIN_STEP_ON);
+		HALFastdigitalWriteNC(MASH6050S_Z_STEP_PIN, MASH6050S_PIN_STEP_ON);
+		HALFastdigitalWriteNC(MASH6050S_C_STEP_PIN, MASH6050S_PIN_STEP_ON);
+	}
+
+	////////////////////////////////////////////////////////
+
+#ifdef USESTEPTIMER
+
+	virtual void Step(const uint8_t steps[NUM_AXIS], axisArray_t directionUp, bool isSameDirection) override
+	{
+		while (_setState != NextIsDone)
 		{
-			CHAL::DelayMicroseconds(5);
-			_lastStepDirection = directionUp;
+			_setState = MyStep(_mysteps, (EnumAsByte(ESetPinState)) _setState, _myCnt);
+			Delay1();
 		}
 
-		for (uint8_t cnt = 0;; cnt++)
+		InitStepDirTimer(steps);
+		if (!isSameDirection)
 		{
-			register bool have = false;
-			if (steps[X_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_X_STEP_PIN, MASH6050S_PIN_STEP_OFF); have = true; }
-			if (steps[Y_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_Y_STEP_PIN, MASH6050S_PIN_STEP_OFF); have = true; }
-			if (steps[Z_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_Z_STEP_PIN, MASH6050S_PIN_STEP_OFF); have = true; }
-			if (steps[A_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_C_STEP_PIN, MASH6050S_PIN_STEP_OFF); have = true; }
+			SetDirection(directionUp);
+			if (MASH6050S_CHANGEDIRECTIONMICROS)
+			{
+				CHAL::StartTimer2OneShot(TIMER2VALUEFROMMICROSEC(MASH6050S_CHANGEDIRECTIONMICROS));
+				// wait until interrupt
+				return;
+			}
+		}
+		HandleStepPinInterrupt();
+	}
 
+	static void HandleStepPinInterrupt()
+	{
+		_setState = MyStep(_mysteps, (EnumAsByte(ESetPinState)) _setState, _myCnt);
+		if (_setState == NextIsDone)
+			CHAL::StopTimer2();
+		else
+			CHAL::StartTimer2OneShot(TIMER2VALUEFROMMICROSEC(STEPTIMERDELAYINMICRO));
+	}
+
+	static EnumAsByte(ESetPinState) MyStep(const uint8_t steps[NUM_AXIS], EnumAsByte(ESetPinState) state, uint8_t& cnt)
+	{
+		if (state == NextIsSetPin)
+		{
+			SetStepPin(steps, cnt);
+			cnt++;
+
+			state = AnyPendingAxis(steps, cnt) ? NextIsClearPin : NextIsClearDonePin;
+		}
+		else
+		{
+			ClearStepPin();
+			state = state == NextIsClearPin ? NextIsSetPin : NextIsDone;
+		}
+		return state;
+	}
+
+	static bool AnyPendingAxis(const uint8_t steps[NUM_AXIS], uint8_t cnt)
+	{
+		// optimize => not used as "Delay1
+		return steps[X_AXIS] > cnt || steps[Y_AXIS] > cnt || steps[Z_AXIS] > cnt || steps[A_AXIS] > cnt;
+	}
+
+#else
+
+	virtual void Step(const uint8_t steps[NUM_AXIS], axisArray_t directionUp, bool isSameDirection) override
+	{
+		if (!isSameDirection)
+		{
+			SetDirection(directionUp);
+			if (MASH6050S_CHANGEDIRECTIONMICROS)
+			{
+				CHAL::DelayMicroseconds(MASH6050S_CHANGEDIRECTIONMICROS);
+			}
+		}
+
+		for (uint8_t cnt = 0;;)
+		{
+			SetStepPin(steps, cnt);
+			cnt++;
+
+			// AVR: GetPendingAxis will take 6-8 machine cycle per axis: 0.5 us
+			//      Delay1() may be very short (=0 if axis >= 2)
+			uint8_t pending = GetPendingAxis(steps, cnt);
 			Delay1();
 
-			if (steps[X_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_X_STEP_PIN, MASH6050S_PIN_STEP_ON); }
-			if (steps[Y_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_Y_STEP_PIN, MASH6050S_PIN_STEP_ON); }
-			if (steps[Z_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_Z_STEP_PIN, MASH6050S_PIN_STEP_ON); }
-			if (steps[A_AXIS] > cnt) { HALFastdigitalWriteNC(MASH6050S_C_STEP_PIN, MASH6050S_PIN_STEP_ON); }
+			ClearStepPin();
 
-			if (!have) break;
+			if (pending == 0)
+				break;
 
 			Delay2();
 		}
 	}
 
-public:
+	static uint8_t GetPendingAxis(const uint8_t steps[NUM_AXIS], uint8_t cnt)
+	{
+		// do not optimize => it will be used as "Delay1
+		uint8_t pending = 0;
+		if (steps[X_AXIS] > cnt) { pending++; }
+		if (steps[Y_AXIS] > cnt) { pending++; }
+		if (steps[Z_AXIS] > cnt) { pending++; }
+		if (steps[A_AXIS] > cnt) { pending++; }
+
+		return pending;
+	}
+
+#endif
 
 	////////////////////////////////////////////////////////
+
+public:
 
 	virtual uint8_t GetReferenceValue(uint8_t referenceid) override
 	{
 		switch (referenceid)
 		{
-			case 0: return HALFastdigitalRead(MASH6050S_X_MIN_PIN);
-			case 2: return HALFastdigitalRead(MASH6050S_Y_MIN_PIN);
-			case 5: return HALFastdigitalRead(MASH6050S_Z_MAX_PIN);
-			case 6: return HALFastdigitalRead(MASH6050S_C_MIN_PIN);
+		case 0: return HALFastdigitalRead(MASH6050S_X_MIN_PIN);
+		case 2: return HALFastdigitalRead(MASH6050S_Y_MIN_PIN);
+		case 5: return HALFastdigitalRead(MASH6050S_Z_MAX_PIN);
+		case 6: return HALFastdigitalRead(MASH6050S_C_MIN_PIN);
 		}
 		return 255;
 	}
@@ -183,8 +272,4 @@ public:
 	}
 
 	////////////////////////////////////////////////////////
-
-private:
-
-	axisArray_t		_lastStepDirection;				// for backlash
 };

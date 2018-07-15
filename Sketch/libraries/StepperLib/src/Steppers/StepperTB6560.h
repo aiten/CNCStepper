5785c99ec70a09f91f0b8b311011d2f8a924994b
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2018-07-15 16:22:41 +0200

    CNCShield without timer2

--- Sketch/libraries/StepperLib/src/Steppers/StepperTB6560.h
+++ Sketch/libraries/StepperLib/src/Steppers/StepperTB6560.h
@@ -134,7 +134,7 @@ protected:
 
 	////////////////////////////////////////////////////////
 
-	virtual void  Stepuint8_t steps[NUM_AXIS], uint8_t directionUp) override
+	virtual void Step(const uint8_t steps[NUM_AXIS], axisArray_t directionUp, bool isSameDirection) override
 	{
 		// Step:   LOW to HIGH
 

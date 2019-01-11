c35cccd2fb565909ac0a61a2aa7f89cb81641ab6
Herbert Aitenbichler <h.aitenbichler@eduhi.at>
2019-01-11 08:19:47 +0100

    (c) 2019

--- VS/Tools/CreateThermistorLookupBeta/CreateThermistorLookupBeta/Properties/AssemblyInfo.cs
+++ VS/Tools/CreateThermistorLookupBeta/CreateThermistorLookupBeta/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CreateThermistorLookupBeta")]
-[assembly: AssemblyCopyright("Copyright ©  2013-2017")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
